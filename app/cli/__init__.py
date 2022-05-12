50bf8e3ccf5bd9d523c12b0a2f7661182acc8fc3
jtn24-NJIT <jtn24@njit.edu>
2022-05-11 23:56:24 -0400

    Format fixes

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -6,11 +6,11 @@ from app.db import db
 @click.command(name='create-db')
 @with_appcontext
 def create_database():
-    # get root directory of project
+    # Get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
+    # Set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../../database')
-    # make a directory if it doesn't exist
+    # Make a directory if it doesn't exist
     if not os.path.exists(dbdir):
         os.mkdir(dbdir)
     db.create_all()
@@ -18,10 +18,10 @@ def create_database():
 @click.command(name='create-log-folder')
 @with_appcontext
 def create_log_folder():
-#Get root directory of project
+    # Get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
+    # Set the name of the apps log folder to logs
     logdir = os.path.join(root, '../../logs')
-    # make a directory if it doesn't exist
+    # Make a directory if it doesn't exist
     if not os.path.exists(logdir):
         os.mkdir(logdir)
